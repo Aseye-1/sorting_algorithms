eba7273dc7fabc63977f608dba3de7b1bf483dbf
KBryt <effahbright.eb@gmail.com>
2023-04-24 17:29:21 -0700

    update bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,19 @@
 #include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
 /**
  * bubble_sort - sort array lements from min to max value
  * @array: array
